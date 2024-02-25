9ae27a7adfb7d4fbd98e29f7a9f23af9254882aa
Juan Prado <juan@juanpabloprado.com>
2024-02-24 21:24:41 -0600

    add closures

--- src/main/scala/functions/SuperHeroes.scala
+++ src/main/scala/functions/SuperHeroes.scala
@@ -3,81 +3,17 @@ package functions
 
 object SuperHeroes {
   def main(args: Array[String]): Unit = {
+    var prefix = "Lighting "
 
-    val heroes = Array("Wonder Woman", "Thor", "Superman", "Batman")
+    def reverseIt(name: String) = (name + prefix).reverse
 
-    val lowerC = (value: String) => value.toLowerCase
+    val thor = reverseIt("Thor")
 
-//    for (elem <- heroes) {
-//      println(lowerC(elem))
-//    }
+    prefix = "Super Fast "
 
-    def updateRankings(x: Int) = x - 3
+    val wonderWoman = reverseIt("Wonder Woman")
 
-//    (x: Int) => x - 3
-
-    val avengers = Map("Iron Man" -> 12, "Black Widow" -> 18, "Captain America" -> 8, "Scarlet Witch" -> 10)
-
-    val newRankings = avengers.values.map((x: Int) => x - 3)
-
-//    newRankings.foreach(println)
-
-    def multiply(x: Int, y: Int, z: Int) = x * y * z
-
-    val result = multiply(1, 2, 3)
-//    println(result)
-
-    val someResult = multiply(1, 2, _:Int)
-//    println(someResult(3))
-
-    val nonResult = multiply
-//    println(nonResult(1, 2, 3))
-
-    def add(x: Int) = (y: Int) => x + y
-//    val sum = add(8)
-//    println(sum(2))
-
-//    println(add(2)(8))
-
-    def addition(x: Int)(y: Int) = x + y
-
-//    println(addition(10)(12))
-
-//    heroes.foreach(println)
-    val lHeroes = heroes.map(data => data.toLowerCase)
-
-    val lHeroes1 = heroes.map(_.toLowerCase)
-//    lHeroes.foreach(println)
-
-//    val man = heroes.filter(_.contains("man"))
-//    man.foreach(println)
-
-    val man = heroes.filter(_.contains("man")).map(_.toUpperCase)
-
-    man.foreach(println)
-
-    val numbers = Array(1,2,3,4,5,4,4,3,5)
-
-    val less = numbers.dropWhile(_ < 3)
-//    less.foreach(println)
-
-    val howMany = numbers.count(_ <= 4)
-//    println(howMany)
-
-    val isItInThere = numbers.indexWhere(_ == 3)
-    println(isItInThere)
-
-    val ironManR = avengers.getOrElse("Iron Man", 0)
-    val blackWidowR = avengers.getOrElse("Black Widow", 0)
-    val captainAmericaR = avengers.getOrElse("Captain America", 0)
-
-    def compare(avenger1: Int, avenger2: Int, avenger3: Int,
-                f: (Int, Int, Int) => Int) = f(avenger1, avenger2, avenger3)
-
-    val bestRanked = compare(ironManR, blackWidowR, captainAmericaR, _ min _ min _)
-//    println(bestRanked)
-
-    val worstRanked = compare(ironManR, blackWidowR, captainAmericaR, _ max _ max _)
-    println(worstRanked)
+    println(thor)
+    println(wonderWoman)
   }
 }
